233531667275f6414a875b35264cb8f20cf56d09
Irfan Baqui <irfan.baqui@gmail.com>
2015-08-30 15:11:12 -0700

    modify travis_setup script

--- travis_setup.sh
+++ travis_setup.sh
@@ -12,14 +12,14 @@ then
   git clone https://github.com/IrfanBaqui/travis2
   cd travis
   git checkout latest_release
-  git checkout -b backward_compatibility_test
+  # git checkout -b backward_compatibility_test
   git rm -r migrations
   git checkout remotes/origin/migration_change -- migrations
   npm install
   npm test;
 fi
 
-if [[ DIR_CHANGES != *"/migrations"* ]]
+if [[ $DIR_CHANGES != *"/migrations"* ]]
 then
   echo "It's NOT there!";
 fi
