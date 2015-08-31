6fd5a3fad6683b2dd12e6e692f9cef57a31fb9ba
Irfan Baqui <irfan.baqui@gmail.com>
2015-08-31 11:05:42 -0700

    changed travis setup

--- travis_setup.sh
+++ travis_setup.sh
@@ -9,34 +9,69 @@ set -v
 # Run tests
 npm test
 
-CURR_BRANCH=$( git rev-parse HEAD )
-echo $TRAVIS_BRANCH
-echo $TRAVIS_COMMIT
-echo $TRAVIS_PULL_REQUEST
-echo $TRAVIS_TAG
+if [[ $TRAVIS_PULL_REQUEST ]]
+then
 
-# git rev-parse remotes/origin/latest_release
-# git rev-parse remotes/origin/migration_change
-DIR_CHANGES=$( git show --name-only --pretty=oneline )
+	CURR_BRANCH=$( git rev-parse HEAD )
+	echo $TRAVIS_BRANCH #migration_change
+	echo $TRAVIS_COMMIT
+	echo $TRAVIS_PULL_REQUEST
+	echo $TRAVIS_TAG
 
-# if [[ $DIR_CHANGES = *"migrations/"* ]]
-# then
-  # echo "It's there!"
-  git clone https://github.com/IrfanBaqui/travis2
-  cd travis2
-  git checkout remotes/origin/latest_release
-  # git diff --name-status --color remotes/origin/latest_release..remotes/origin/migration_change
-  git diff --name-status --color remotes/origin/latest_release..$CURR_BRANCH
-  # git checkout -b backward_compatibility_test
-  git rm -r migrations
-  # git checkout remotes/origin/migration_change -- migrations
-  git checkout $CURR_BRANCH -- migrations
-  ls
-  cat ./migrations/migrations_file.js
-  echo "syntax error" >> ./migrations/migrations_file.js
-  cat ./migrations/migrations_file.js
-  npm install
-  npm test;
-# fi
+	# git rev-parse remotes/origin/latest_release
+	# git rev-parse remotes/origin/migration_change
+	DIR_CHANGES=$( git show --name-only --pretty=oneline )
+
+	# if [[ $DIR_CHANGES = *"migrations/"* ]]
+	# then
+	  # echo "It's there!"
+	  git clone https://github.com/IrfanBaqui/travis2
+	  cd travis2
+	  git checkout remotes/origin/latest_release
+	  # git diff --name-status --color remotes/origin/latest_release..remotes/origin/migration_change
+	  git diff --name-status --color remotes/origin/latest_release..$CURR_BRANCH
+	  # git checkout -b backward_compatibility_test
+	  git rm -r migrations
+	  # git checkout remotes/origin/migration_change -- migrations
+	  git checkout $CURR_BRANCH -- migrations
+	  ls
+	  cat ./migrations/migrations_file.js
+	  echo "syntax error" >> ./migrations/migrations_file.js
+	  cat ./migrations/migrations_file.js
+	  npm install
+	  npm test;
+	# fi
+else 
+	CURR_BRANCH=$( git rev-parse HEAD )
+	git branch -a
+	echo $TRAVIS_BRANCH #develop
+	echo $TRAVIS_COMMIT
+	echo $TRAVIS_PULL_REQUEST
+	echo $TRAVIS_TAG
+
+	# git rev-parse remotes/origin/latest_release
+	# git rev-parse remotes/origin/migration_change
+	DIR_CHANGES=$( git show --name-only --pretty=oneline )
+
+	# if [[ $DIR_CHANGES = *"migrations/"* ]]
+	# then
+	  # echo "It's there!"
+	  git clone https://github.com/IrfanBaqui/travis2
+	  cd travis2
+	  git checkout remotes/origin/latest_release
+	  # git diff --name-status --color remotes/origin/latest_release..remotes/origin/migration_change
+	  git diff --name-status --color remotes/origin/latest_release..$CURR_BRANCH
+	  # git checkout -b backward_compatibility_test
+	  git rm -r migrations
+	  # git checkout remotes/origin/migration_change -- migrations
+	  git checkout $CURR_BRANCH -- migrations
+	  ls
+	  cat ./migrations/migrations_file.js
+	  echo "syntax error" >> ./migrations/migrations_file.js
+	  cat ./migrations/migrations_file.js
+	  npm install
+	  npm test;
+	# fi
+fi
 
 exit 0
